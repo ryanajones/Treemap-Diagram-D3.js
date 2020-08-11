c114cf5ab43e4b88a58fee1653fa4eaaba7e93ef
Ryan Jones <ryguy243@outlook.com>
2020-08-10 20:53:16 -0700

    set up color scale, treemap, cells, and tiles.

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
+// Pre define necessary variables
+
 const w = 1200;
 const h = 770;
 const padding = 60;
@@ -21,6 +23,26 @@ const svg = d3
   .style('border-bottom-right-radius', '10px')
   .style('border-bottom-left-radius', '10px');
 
+const fader = (colors) => d3.interpolateRgb(colors, '#fff')(0.2);
+console.log(fader);
+/* const color = d3.scaleOrdinal(d3.schemeCategory20.map(fader));
+ */
+const treemap = d3.treemap().size([w, h]);
+
 // Define json call
 
-d3.json();
+d3.json(
+  'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json'
+).then((data) => {
+  const root = d3.hierarchy(data).sum((d) => d.value);
+  treemap(root);
+
+  const cell = svg.selectAll('g').data(root.leaves()).enter().append('g');
+
+  const tile = cell
+    .append('rect')
+    .attr('width', (d) => d.x1 - d.x0)
+    .attr('height', (d) => d.y1 - d.y0);
+  /*     .attr('fill', (d3) => color(d.data.category));
+   */
+});
